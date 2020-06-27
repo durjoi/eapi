aad607231b11c67d67b2d5f16eed62e9b42e370d
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-06-27 11:00:20 +0600

    Migration created for products and review

--- database/migrations/2020_06_27_041600_create_reviews_table.php
+++ database/migrations/2020_06_27_041600_create_reviews_table.php
@@ -15,7 +15,13 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
