0b4a82e31cc5473c5c4455e6af919f1bcdbc34da
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-06-27 10:53:15 +0600

    Created Model - a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('products', 'Api\ProductController');
+
+Route::group(['prefix'=>'products'], function(){
+  Route::apiResource('/{product}/reviews', 'Api\ReviewController');
+});
